fe664cc1c91f5690a7849611a99c57e5534f0647
mykola.pron <mykola.pron.dev@gmail.com>
2023-09-17 12:17:35 +0300

    added images and styles

--- src/components/LaunchList.js
+++ src/components/LaunchList.js
@@ -4,11 +4,25 @@ const LaunchList = (props) => {
     return (
         <div>
             <h2>List</h2>
-            <div>
+            <div style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(3,1fr)",
+                gap: "10px"
+            }}>
                 {props.launches.map((launch) => {
                     const date = new Date(launch.date_unix)
                     return (
-                        <div key={launch.id}>
+                        <div
+                            key={launch.id}
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                cursor: "pointer"
+                            }}
+                        >
+                            <img src={launch.links.patch.small} alt=""/>
                             <div>Name: {launch.name}</div>
                             <div>Flight Number: {launch.flight_number}</div>
                             <div>Date: {date.toLocaleDateString()}</div>
